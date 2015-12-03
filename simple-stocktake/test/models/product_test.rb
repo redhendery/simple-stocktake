acdfe6a68b8957bc2f63064a2bdc5f01a5b50593
Red Hendery <red.hendery@gmail.com>
2015-12-03 23:34:50 +1300

    migrate DB

--- simple-stocktake/test/models/product_test.rb
+++ simple-stocktake/test/models/product_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save new product without name" do
+    product = Product.new
+    assert_not product.save
+  end
+
 end
